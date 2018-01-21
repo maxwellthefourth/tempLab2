30a1cba277056fa56ce97149e2859882601e7528
maxwellthefourth <maxwellthefourth@maxwellthefourth-VirtualBox>
2018-01-20 18:16:32 -0700

    test commit

--- ProcessTrace.cpp
+++ ProcessTrace.cpp
@@ -8,29 +8,49 @@
 #include "ProcessTrace.h"
 
 ProcessTrace::ProcessTrace(string executionFile) {
-    
     // Open execution trace file
     inFile.open(executionFile);
-    if (inFile.fail()) { // If not successful, exits program
+    if (inFile.fail()) { // Exit program if it can't open file
         cerr << "ERROR: Cannot open text file: " << executionFile << endl;
         exit(2);
     }
     cout << "File opened successfully." << endl;
-    
-    
 }
 
 void ProcessTrace::Execute() {
     // allocate an empty array as an std::vector<uint8_t> type
     std::vector<uint8_t> arr(0);
+    
     // execute the commands in the trace file, using the vector as the memory referenced by the commands
-    switch(2) {
-        case 1: cout << "1" << endl;
-                break;
-        case 2: cout << "2" << endl;
-                break;
-        default: cout << "default" << endl;
-                break;
+    string tempLine, tempWord;
+    while (getline(inFile, tempLine)) { // Go through line by line
+        cout << tempLine << endl;
+        std::istringstream iss(tempLine);
+        while (iss >> tempWord) { // Look at the first word of the line
+            if (tempWord == "alloc") {
+                int val;
+                iss >> std::hex >> val; // Converts value from hex to dec
+                arr.resize(val);
+            }
+            else if (tempWord == "compare") {
+                
+            }
+            else if (tempWord == "put") {
+                
+            }
+            else if (tempWord == "fill") {
+                
+            }
+            else if (tempWord == "copy") {
+                
+            }
+            else if (tempWord == "dump") {
+                
+            }
+            else {
+                // default
+            }
+        }
     }
 }
 
