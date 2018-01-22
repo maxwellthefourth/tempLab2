420c2b0216af0a69073c74f93074ad68022c6904
maxwellthefourth <maxwellthefourth@maxwellthefourth-VirtualBox>
2018-01-21 17:21:14 -0700

    random bits

--- ProcessTrace.cpp
+++ ProcessTrace.cpp
@@ -1,13 +1,18 @@
 /* 
  * File:   ProcessTrace.cpp
- * Author: maxwellthefourth
+ * Author: Grant Kochmann and Maxwell Evans
  * 
  * Created on January 20, 2018, 4:12 PM
  */
 
 #include "ProcessTrace.h"
-#include <iomanip>
 
+  /**
+   * ProcessTrace - Constructor for ProcessTrace class
+   * 
+   * @param executionFile     Name of file to be opened
+   * @throws                  Throws exception on error opening file
+   */
 ProcessTrace::ProcessTrace(string executionFile) {
     // Open execution trace file
     inFile.open(executionFile);
@@ -15,9 +20,14 @@ ProcessTrace::ProcessTrace(string executionFile) {
         cerr << "ERROR: Cannot open text file: " << executionFile << endl;
         exit(2);
     }
-    cout << "File opened successfully." << endl;
 }
 
+  /**
+   * Execute - creates empty array of type uint8_t vector, reads the input trace file and acts on commands from the file
+   * Commands include alloc, compare, put, fill, copy, and dump
+   * 
+   * @throws Throws exception if the file type is invalid (as in it doesn't use one of the six commands on one of the lines)
+   */
 void ProcessTrace::Execute() {
     // allocate an empty array as an std::vector<uint8_t> type
     std::vector<uint8_t> arr;
@@ -37,8 +47,8 @@ void ProcessTrace::Execute() {
             else if (tempWord == "compare") {
                 unsigned int addr, val;
                 iss >> std::hex >> addr;
-                while (iss >> val) {
-                    if (arr[addr] != val) {
+                while (iss >> val) { // Goes through each expected value in the line
+                    if (arr[addr] != val) { // If the value does not match what was expected, outputs an error
                         cout << "compare error at address " << std::hex << addr << ", expected " << std::hex << val << ", actual " << std::hex << unsigned(arr[addr]) << endl;
                     }
                     addr++;
@@ -47,22 +57,22 @@ void ProcessTrace::Execute() {
             else if (tempWord == "put") {
                 unsigned int addr, val;
                 iss >> std::hex >> addr;
-                while (iss >> val) {
+                while (iss >> val) { // Goes through each value in the line and puts them in order into the vector at the address
                     arr[addr] = val;
                     addr++;
                 }
             }
             else if (tempWord == "fill") {
                 unsigned int addr, count, val;
-                iss >> std::hex >> addr;
+                iss >> std::hex >> addr; // First three values are the address, the amount of numbers to put, and the value to be put in
                 iss >> std::hex >> count;
                 iss >> std::hex >> val;
-                for (int i = 0; i < count; i++)
+                for (int i = 0; i < count; i++) // For loop to put in values at address
                     arr[addr+i] = val;
             }
             else if (tempWord == "copy") {
                 unsigned int dest_addr, src_addr, count;
-                iss >> std::hex >> dest_addr;
+                iss >> std::hex >> dest_addr; // First three values are the destination address, the source address, and the amount of numbers to copy
                 iss >> std::hex >> src_addr;
                 iss >> std::hex >> count;
                 for (int i = 0; i < count; i++)
@@ -70,20 +80,21 @@ void ProcessTrace::Execute() {
             }
             else if (tempWord == "dump") {
                 unsigned int addr, count;
-                iss >> std::hex >> addr;
+                iss >> std::hex >> addr; // First two values are the address and the amount to dump
                 iss >> std::hex >> count;
                 cout << addr << endl;
                 int i = 0;
-                for (; i < count; i++) {
-                    cout << " " << std::setw(2) << std::setfill('0') << std::hex << unsigned(arr[i+addr]);
-                    if (i % 16 == 15)
+                for (; i < count; i++) { // For loop to go through each value starting from address
+                    cout << " " << std::setw(2) << std::setfill('0') << std::hex << unsigned(arr[i+addr]); // setw(2) sets it so the minimum number of digits is 2 and setfill('0') to make sure a 0 is put where there would be an extra digit. The number is converted to unsigned so that it appears in the output
+                    if (i % 16 == 15) // New line every 16 values
                         cout << endl;
                 }
-                if (i % 16 != 0)
+                if (i % 16 != 0) // Only prints another line after the dump if it doesn't end at 16
                     cout << endl;
             }
             else {
-                // default
+                cerr << "Invalid file type - cannot execute trace file" << endl;
+                exit(2);
             }
         }
     }
